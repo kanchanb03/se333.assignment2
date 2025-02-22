88337fb550ed5d21431cd7a8eb668fe3d93227d8
kanchanb03 <147580734+kanchanb03@users.noreply.github.com>
2025-02-21 23:43:38 -0600

    after changes

---
+++ Assignment2/src/test/java/org/example/leftPatUtilsTest.java
@@ -0,0 +1,93 @@
+package org.example;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeftPadUtilsTest {
+
+    // ===== SPECIFICATION-BASED TESTS ===== //
+
+    @Test
+    @Tag("specification")
+    void testNull() {
+        assertNull(LeftPadUtils.leftPad(null, 5, "x"));
+    }
+
+    @Test
+    @Tag("specification")
+    void testEmpty() {
+        assertEquals("xx", LeftPadUtils.leftPad("", 2, "x"));
+    }
+
+
+    @Test
+    @Tag("specification")
+    void testpadstrNull() {
+        assertEquals("  a", LeftPadUtils.leftPad("a", 3, null));
+    }
+
+    @Test
+    @Tag("specification")
+    void testPaddingWithEmptySmallSize() {
+        assertEquals("  a", LeftPadUtils.leftPad("a", 3, ""));
+    }
+
+    @Test
+    @Tag("specification")
+    void testPaddingWithOne() {
+        assertEquals("xxxabc", LeftPadUtils.leftPad("abc", 6, "x"));
+    }
+
+
+
+    @Test
+    @Tag("specification")
+    void testPaddingWithemptyLargeSize() {
+        assertEquals("  abc", LeftPadUtils.leftPad("abc", 5, " "));
+    }
+
+    // ===== COVERAGE-BASED TESTS ===== //
+
+
+    @Test
+    @Tag("coverage")
+    void testPaddingWithlessSize() {
+        assertEquals("abc", LeftPadUtils.leftPad("abc", 3, "xyz"));
+    }
+
+
+    @Test
+    @Tag("coverage")
+    void testWithSameLength() {
+        assertEquals("xyzabc", LeftPadUtils.leftPad("abc", 6, "xyz"));
+    }
+
+
+    @Test
+    @Tag("coverage")
+    void testEmptyString() {
+        assertEquals("abc", LeftPadUtils.leftPad("abc", 1, ""));
+    }
+
+    @Test
+    @Tag("coverage")
+    void testSizeWithNull() {
+        assertEquals("abc", LeftPadUtils.leftPad("abc", 2, null));
+    }
+    @Test
+    public void testLeftPadBoundaries() {
+        String original = "cat";
+        String result = LeftPadUtils.leftPad(original, 3, "XYZ");
+        assertEquals("cat", result, "When size == str.length, no padding");
+
+        original = "cat";
+        result = LeftPadUtils.leftPad(original, 7, "ABCD");
+        assertEquals("ABCDcat", result, "Pads == padLen => padStr.concat(str)");
+
+        original = "cat";
+        result = LeftPadUtils.leftPad(original, 5, "ABCD");
+        assertEquals("ABcat", result,"pads < padLen => substring(0,pads)+str");
+    }
+
+}
